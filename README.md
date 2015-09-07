7de86384a72e20a8a5c3c885f16978c70c9258b4
ackuser <mail@ackarim.com>
2015-09-07 11:05:23 +0100

    Fixing get, post, bad request and adding path module

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# node_basic_server
-Basic Nodejs Server with different functionalities
+# Basic Nodejs Server
+
+Basic Nodejs Server is a basic example of how a Nodejs Server can be implemented. Among their functionalities:
+
+  - Basic GET returning a static file index.html
+  - Basic POST extracting data sent to the server a returning it back to the client on the index.html
+  - Response code
+  - Checking url paths are corretcg
+
+Markdown is a lightweight markup language based on the formatting conventions that people naturally use in email.  As [John Gruber] writes on the [Markdown site] [1]:
+
+> The overriding design goal for Markdown's
+> formatting syntax is to make it as readable
+> as possible. The idea is that a
+> Markdown-formatted document should be
+> publishable as-is, as plain text, without
+> looking like it's been marked up with tags
+> or formatting instructions.
+
+This text you see here is *actually* written in Markdown! To get a feel for Markdown's syntax, type some text into the left window and watch the results in the right.
+
+### Version
+1.0.0
+
+Basic Nodejs Server is an example of a server of Nodejs with a [public repository](https://github.com/ackuser/node_basic_server) on GitHub.
+
+### Installation
+
+You need :
+
+```sh
+$ git clone [git-repo-url] node_basic_server
+$ cd node_basic_server
+$ npm install
+```
+Run server:
+```sh
+$ node index.js
+```
+Run client or going to http://localhost:3000, different get and post requests
+```sh
+$ curl http://localhost:3000
+$ curl http://localhost:3000/index.html
+$ curl -d 'this a post request' http://localhost:3000
+$ curl -d 'this a post request' http://localhost:3000/index.html
+```
+
+License
+----
+
+GPL-3.0
+
+
+**Si lo que vas a decir no es más bello que el silencio: no lo digas (Proverbio árabe)**
+
+- [@ackuser](http://twitter.com/ackuser)
--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 var http = require('http');
 var fs = require('fs');
-var url_parser = require('url');
+var parse = require('url').parse;
+var join = require('path').join;
 var items = [];
+var root = __dirname;
 
 var server = http.createServer(function(req, res){
-  var url = url_parser.parse(req.url);
-  if ('/' == req.url) {
+  var url = parse(req.url);
+  if ('/index.html' == req.url || '/' == req.url) {
     switch (req.method) {
       case 'GET':
-        console.log('I have received a external GET request from ' + url.href);
-        fs.readFile('index.html',function (err, data){
+        console.log('I have received a external GET request from ' + JSON.stringify(url) );
+        //console.log(JSON.stringify(req, null, 4));
+        var path = join(root, url.pathname);
+        if ('/' == req.url){
+          path = join(root, 'index.html');
+          //console.log('I have received a external GET request from /');
+          //console.log(path);
+        }
+        fs.readFile(path,function (err, data){
           res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
           res.write(data);
           res.end();
         });
         break;
         case 'POST':
-          console.log('I have received a external POST request from ' + url);
-          add(req, res);
+          console.log('I have received a POST request from ' + JSON.stringify(url));
+          var body = '';
+          req.setEncoding('utf8');
+          req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+          });
+          req.on('end', function () {
+            console.log("Body: " + body);
+          res.writeHead(200, {'Content-Type': 'text/html','Content-Length':body.length});
+          res.write(body);
+          res.end();
+          });
           break;
           default:
             console.log('Bad request');
@@ -26,7 +46,10 @@ var server = http.createServer(function(req, res){
             //badRequest(res);
           }
         } else {
-          notFound(res);
+          console.log('Bad path');
+          console.log('I have received a external request from ' + req.url + ' ' + JSON.stringify(url) );
+          res.statusCode = 404;
+          res.end('Not Found');
         }
       });
       console.log('Hello, Nodejs Server listening on 3000');
